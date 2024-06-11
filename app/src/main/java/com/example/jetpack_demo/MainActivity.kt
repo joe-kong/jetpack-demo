1c2ab33062d14b017875be74792e3f45b4e83759
koureijun <koras7788@gmail.com>
2024-06-11 23:00:52 +0900

    animation
    add animation to list expand

--- app/src/main/java/com/example/jetpack_demo/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_demo/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,6 +23,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -50,7 +56,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackdemoTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MessageCard(msg = Message("Android","Jetpack Compose"))
+                   // MessageCard(msg = Message("Android","Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -100,7 +107,12 @@ fun MessageCard(msg: Message) {
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(20.dp))
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column (modifier = Modifier.clickable { isExpanded = !isExpanded }){
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
@@ -112,19 +124,28 @@ fun MessageCard(msg: Message) {
 //                text = msg.body,
 //                style = MaterialTheme.typography.bodyMedium
 //                )
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 10.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 10.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = MaterialTheme.colorScheme.surface,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
-                )
-            }
+                   )
+               }
+           }
         }
 
     }
-
-
-}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
